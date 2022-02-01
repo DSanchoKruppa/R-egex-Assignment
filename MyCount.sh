f707c9c19b21f7d82571d07ad2bc2b5c0c792283
DSanchoKruppa <dsanchokruppa@gmail.com>
2022-02-01 13:05:31 -0500

    Finalized bash script

--- MyCount.sh
+++ MyCount.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
+# This script loops through numbers 1-20 and at each iteration prints the loop
+# iteration number and whether the number has single or double digits
 for Count in {1..20}
 do
-  if [ $((Count - 9)) -le 0 ] 
+  if [ $((Count - 9)) -le 0 ] # every single digit number <= 9
   then
     echo "$Count has one, lonely, single digit"
   else
