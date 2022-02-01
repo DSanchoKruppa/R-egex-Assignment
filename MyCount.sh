bd947c745eacedf1b49b0bc71f41190b5e58b2ff
DSanchoKruppa <dsanchokruppa@gmail.com>
2022-02-01 13:13:43 -0500

    Finalized regex and bash scripts, as well as knitting the markdown to pdf

--- MyCount.sh
+++ MyCount.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
 
+# February 2, 2022
+# Diego Sancho Kruppa 20101377
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 # This script loops through numbers 1-20 and at each iteration prints the loop
-# iteration number and whether the number has single or double digits
+# iteration number and whether the number has single or double digits.
+
 for Count in {1..20}
 do
-  if [ $((Count - 9)) -le 0 ] # every single digit number <= 9
+  if [ $((Count - 9)) -le 0 ] # every single digit number <= 9 (so I'm told)
   then
-    echo "$Count has one, lonely, single digit"
+    echo "$Count has one, lonely, single digit" # poor guy
   else
     echo "$Count has a jolly pair of double digits"
   fi
